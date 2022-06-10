279c7a0347e4112db15e55cf9e31190a25f34f09
ariel.ruiz <ariel.ruiz@encora.com>
2022-06-10 15:31:07 -0600

    Basic code

---
+++ Basic/map.dart
@@ -0,0 +1,24 @@
+void main() {
+  
+//   Map persona = {
+//     'nombre': 'Fernando',
+//     'edad': 35,
+//     'soltero': false,
+//     true: false,
+//     1: 100,
+//     2: 500
+//   };
+
+  print(persona['nombre');//Fernando
+  print(persona[2);//500
+  
+  Map<String, dynamic> persona = {
+    'nombre': 'Fernando',
+    'edad': 35,
+    'soltero': false,
+  };
+  
+  persona.addAll({ 'segundoNombre': 'Juan' });
+  
+  print( persona );
+}
